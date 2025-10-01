8ae2df3832a4138e44b8b17649c885cc45703b5b
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-10-01 18:54:42 -0400

    Archivos Base del proyecto

---
+++ app/src/main/java/edu/ucne/francis_castillo_ap2_1/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.ucne.francis_castillo_ap2_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.francis_castillo_ap2_1.ui.theme.Francis_castillo_ap2_1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Francis_castillo_ap2_1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Francis_castillo_ap2_1Theme {
+        Greeting("Android")
+    }
+}
